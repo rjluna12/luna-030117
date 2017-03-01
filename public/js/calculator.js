cd6884d661feb6c281bad8dc4aa62d3d38534631
Kimberly <barcenakim246@gmail.com>
2017-03-01 21:13:35 +0800

    RAS GAMING

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -2,5 +2,5 @@ function addme(){
   var x = document.getElementById("fn").value;
   var y = document.getElementById("sn").value;
   var sum = parseInt(x) + parseInt(y);
-	document.getElementById("answer").innerHTML =sum;
+	document.getElementById("answer").innerHTML = sum;
 }
